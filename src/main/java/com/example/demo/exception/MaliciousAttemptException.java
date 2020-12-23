354ba9d6ceffeb86bc3745ebf8b87dd2a79acfe2
Jimmy Thinkpad <jimmy@thinkpad.com>
2020-12-23 19:43:37 +0200

    fixup! user crud pre authorize

--- src/main/java/com/example/demo/exception/MaliciousAttemptException.java
+++ src/main/java/com/example/demo/exception/MaliciousAttemptException.java
@@ -1,7 +1,10 @@
 package com.example.demo.exception;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.security.core.AuthenticationException;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
+@ResponseStatus(HttpStatus.BAD_REQUEST)
 public class MaliciousAttemptException extends AuthenticationException {
 
     public MaliciousAttemptException(String msg, Throwable cause) {
